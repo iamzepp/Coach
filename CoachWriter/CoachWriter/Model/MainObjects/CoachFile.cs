2b842a272058afd8f89027b981bdd0ae9760c3fc
Anatoliy Ignatev <anatoliyignatev@gmail.com>
2019-12-12 20:45:41 +0300

    Add new classes

---
+++ CoachWriter/CoachWriter/Model/MainObjects/CoachFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoachWriter.Model.MainObjects
+{
+    public class CoachFile
+    {
+        public int Id { get; set; }
+
+        public List<Instruction> { get; set; }
+
+    }
+}
---
+++ CoachWriter/CoachWriter/Model/MainObjects/Instruction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoachWriter.Model.MainObjects
+{
+    public class Instruction
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Place { get; set; }
+        public string Effect { get; set; }
+        public string Value { get; set; }
+        public string Instr { get; set; }
+        public string Group { get; set; }
+        public string Clast { get; set; }
+    }
+}
